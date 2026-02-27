92176ccabb5f341828e2beb98201a1765b046cc6
codersunited1459 <believe5149@gmail.com>
2026-02-27 19:55:59 +0530

    initial commit

---
+++ src/test/java/org/mapping/MappingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
